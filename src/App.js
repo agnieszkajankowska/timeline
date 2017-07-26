cca1f31b9684b88a384388b57e02a9c393793595
agnieszkajankowska <ajankowska0404@gmail.com>
2017-07-26 22:40:12 +0200

    adding data to load to timeline view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Timeline from 'react-calendar-timeline'
 import moment from 'moment'
 import './App.css';
+
 moment().format();
 
-const groups = [
-    {id: 1, title: 'group 1'},
-    {id: 2, title: 'group 2'}
-]
+
+
 
 const items = [
     {id: 1, group: 1, title: 'item 1', start_time: moment(), end_time: moment().add(1, 'hour')},
@@ -16,18 +15,34 @@ const items = [
 ]
 
 class App extends Component {
-  render() {
-      console.log('xxx')
-    return (
-      <div className="App">
-        <Timeline groups={groups}
-                  items={items}
-                  defaultTimeStart={moment().add(-12, 'hour')}
-                  defaultTimeEnd={moment().add(12, 'hour')}
-                  />
-      </div>
-    );
-  }
+    constructor() {
+        super()
+        this.state = {
+            timelineData: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(process.env.PUBLIC_URL + 'data/timelineData.json')
+            .then(response => response.json())
+            .then(data => this.setState({timelineData: data}))
+
+    }
+
+    render() {
+        const projects = this.state.timelineData.map( project => ({id: project.id, title: project.name}))
+
+        console.log(this.state.timelineData)
+        return (
+            <div className="App">
+                <Timeline groups={projects}
+                          items={items}
+                          defaultTimeStart={moment().add(-12, 'hour')}
+                          defaultTimeEnd={moment().add(12, 'hour')}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
